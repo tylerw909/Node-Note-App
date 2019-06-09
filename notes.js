4d77229700d2fc769608d20164ecddb0898ec8da
Tyler Weisner <tylerw909@gmail.com>
2019-06-09 16:15:14 -0400

    Intro to Node.js and practice working with NODE.js, communicating between files, functions, objects, and Node.js syntax

---
+++ notes.js
@@ -0,0 +1,78 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+const getNotes = () => {
+    'Your notes...'
+}
+
+const addNote = (title, body) => {
+    const notes = loadNotes()
+    const duplicateNote = notes.find((note) => note.title === title)
+
+    if (!duplicateNote) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log(chalk.green.inverse('New note added!'))
+    } else {
+        console.log(chalk.red.inverse('Note title taken!'))
+    }
+}
+
+const removeNote = (title) => {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter( (note) => note.title !== title)
+
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.green.inverse('Note removed!'))
+        saveNotes(notesToKeep)
+    } else {
+        console.log(chalk.red.inverse('No note found!'))
+    }    
+}
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+const listNotes = () => {
+    const notes = loadNotes()
+    console.log(chalk.inverse('Your Notes:'))
+    
+    notes.forEach((note) => {
+        console.log(note.title)
+    })
+}
+
+const readNote = (title) => {
+    const notes = loadNotes()
+    const foundNote = notes.find((note) => note.title === title)
+
+    if (foundNote) {
+        console.log(chalk.white.inverse(foundNote.title))
+        console.log(foundNote.body)
+    } else {
+        console.log(chalk.red.inverse('Note DNE!'))
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes,
+    readNote: readNote
+}
